85697ee1da94a5e5fb38f0a8ed7fbe5d0415ec8e
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-05-23 14:05:04 +0700

    empty migration

--- backend/simndig/simndig/settings.py
+++ backend/simndig/simndig/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,16 +26,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dosen',
+    'mahasiswa',
+    'authenticate',
 ]
 
 MIDDLEWARE = [
