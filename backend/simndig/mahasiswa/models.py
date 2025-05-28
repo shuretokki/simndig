880e0921bd6d6362de18c59dde5fb79af3745839
justnewbie16 <justnewbie16@gmail.com>
2025-05-29 01:43:01 +0700

    update mahasiswa/models.py

--- backend/simndig/mahasiswa/models.py
+++ backend/simndig/mahasiswa/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Mahasiswa(models.Model):
+    nama = models.CharField(max_length=100)
+    npm = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nama
+
