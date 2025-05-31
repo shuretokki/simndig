e9c7ef1dc60f1c839a48cec0ebb384ff6924ba24
Sir01ok <syauqiihsan30@gmail.com>
2025-05-31 13:34:09 +0700

    auau

--- backend/simndig/matakuliah/urls.py
+++ backend/simndig/matakuliah/urls.py
@@ -4,16 +4,16 @@ from . import views
 app_name = 'matakuliah'
 
 urlpatterns = [
-    # URLs untuk Mahasiswa
-    path('mahasiswa/', views.list_matakuliah_mahasiswa, name='list_matakuliah_mahasiswa'),
-    path('mahasiswa/<int:mk_id>/<str:jenis>/', views.detail_matakuliah_mahasiswa, name='detail_matakuliah_mahasiswa'),
-    path('upload-tugas/<int:tugas_id>/', views.upload_tugas_mahasiswa, name='upload_tugas_mahasiswa'),
-    path('absensi/<int:mk_id>/<str:jenis>/', views.absensi_mahasiswa, name='absensi_mahasiswa'),
-    
-    # URLs untuk Dosen
-    path('dosen/', views.catalog_kelas_dosen, name='catalog_kelas_dosen'),
-    path('dosen/<int:mk_id>/<str:jenis>/', views.kelola_matakuliah_dosen, name='kelola_matakuliah_dosen'),
-    path('dosen/<int:mk_id>/<str:jenis>/tambah-materi/', views.tambah_materi, name='tambah_materi'),
-    path('dosen/<int:mk_id>/<str:jenis>/tambah-tugas/', views.tambah_tugas_dosen, name='tambah_tugas_dosen'),
-    path('dosen/nilai-tugas/<int:tugas_id>/', views.nilai_tugas_dosen, name='nilai_tugas_dosen'),
+    # Mahasiswa URLs
+    path('m/', views.list_matakuliah_mahasiswa, name='list_matakuliah_mahasiswa'),
+    path('m/<int:mk_id>/<str:jenis>/', views.detail_matakuliah_mahasiswa, name='detail_matakuliah_mahasiswa'),
+    path('m/upload/<int:tugas_id>/', views.upload_tugas_mahasiswa, name='upload_tugas_mahasiswa'),
+    path('m/absen/<int:mk_id>/<str:jenis>/', views.absensi_mahasiswa, name='absensi_mahasiswa'),
+
+    # Dosen URLs
+    path('d/', views.catalog_kelas_dosen, name='catalog_kelas_dosen'),
+    path('d/<int:mk_id>/<str:jenis>/', views.kelola_matakuliah_dosen, name='kelola_matakuliah_dosen'),
+    path('d/<int:mk_id>/<str:jenis>/materi/', views.tambah_materi, name='tambah_materi'),
+    path('d/<int:mk_id>/<str:jenis>/tugas/', views.tambah_tugas_dosen, name='tambah_tugas_dosen'),
+    path('d/nilai/<int:tugas_id>/', views.nilai_tugas_dosen, name='nilai_tugas_dosen'),
 ]
