ef0cc4e1ce34ba17d90c7efa7c86b20928516fd1
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-05-31 18:15:11 +0700

    fix dosen/views.py and update styles

--- backend/simndig/dosen/views.py
+++ backend/simndig/dosen/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Dosen
 
+
 @login_required
 def home(request):
     dosen = Dosen.objects.get(user=request.user)
     kelas_list = dosen.kelas.list_dosen.all()
 
     return render(request, 'dosen/home.html', {
-        'user' : dosen.user,
-        'daftar_kelas' : kelas_list
+        'user': dosen.user,
+        'daftar_kelas': kelas_list
     })
 
-def detail_kela(request, kelas_id):
+
+def detail_kelas(request, kelas_id):
     kelas = get_object_or_404(matakuliah, id=kelas_id)
     return render(request, 'dosen/detail_kelas.html', {'kelas': kelas})
