3e894e84e7e59f1682de8a4be97fec5a25bd9b3f
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-06-03 03:08:38 +0700

    update monteli

--- backend/simndig/mahasiswa/forms.py
+++ backend/simndig/mahasiswa/forms.py
@@ -6,29 +6,13 @@ class CompleteMahasiswa(forms.ModelForm):
     class Meta:
         model = Mahasiswa
         # Ensure all desired fields for the initial profile are here
-        fields = ['nama', 'nim', 'email', 'status',
-                  'kelas', 'jurusan', 'angkatan', 'profile_photo']
+        fields = ['nama', 'email',
+                  'profile_photo']
         widgets = {
             'nama': forms.TextInput(attrs={'placeholder': 'Nama Lengkap Anda'}),
-            'nim': forms.TextInput(attrs={'placeholder': 'Nomor Induk Mahasiswa'}),
             'email': forms.EmailInput(attrs={'placeholder': 'contoh@student.kampus.ac.id'}),
-            'status': forms.TextInput(attrs={'placeholder': 'Contoh: Aktif, Cuti'}),
-            'kelas': forms.TextInput(attrs={'placeholder': 'Contoh: TI-2A'}),
-            'jurusan': forms.TextInput(attrs={'placeholder': 'Contoh: Teknik Informatika'}),
-            'angkatan': forms.NumberInput(attrs={'placeholder': 'Contoh: 2022'}),
             # 'profile_photo' will use default FileInput, which is fine
         }
-
-    def clean_nim(self):
-        nim = self.cleaned_data.get('nim')
-        if nim:  # Proceed only if nim is provided
-            qs = Mahasiswa.objects.filter(nim=nim)
-            if self.instance and self.instance.pk:
-                qs = qs.exclude(pk=self.instance.pk)
-            if qs.exists():
-                raise forms.ValidationError(
-                    "NIM sudah terdaftar, silakan gunakan NIM lain.")
-        return nim
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
